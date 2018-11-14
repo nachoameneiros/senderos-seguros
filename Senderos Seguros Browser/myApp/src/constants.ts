95ee898611261445fc47f99e83b0812602c32c53
Unknown <nachoameneiros@hotmail.com>
2018-11-13 23:37:15 -0300

    commit

--- Senderos Seguros Browser/myApp/src/constants.ts
+++ Senderos Seguros Browser/myApp/src/constants.ts
@@ -1,4 +1,5 @@
 export class Constants {
 //   public static API_ENDPOINT= 'http://localhost/';
     public static API_ENDPOINT= 'http://190.19.94.195:8000/';
+//    public static API_ENDPOINT= 'http://54.172.86.107/';
 }
